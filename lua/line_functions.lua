0c2352473e22669c6bfdd098c5393d8b62ec0086
Oleg Skip <skip.cpp.reg@gmail.com>
2022-10-23 00:32:27 +0300

    Add swap line above and below

--- lua/line_functions.lua
+++ lua/line_functions.lua
@@ -8,4 +8,34 @@ function line_functions.insert_line_below()
   vim.api.nvim_put({''}, 'l', true, false)
 end
 
+function line_functions.swap_with_line_above()
+  local current_pos = vim.api.nvim_win_get_cursor(0)
+
+  if current_pos[1] == 1 then -- there is nothing above, nothing to swap
+    return 
+  end
+
+  local line_above = vim.api.nvim_buf_get_lines(0, current_pos[1] - 2, current_pos[1] - 1, false)[1] -- nvim_win_get_cursor is 1-indexed
+  local current_line = vim.api.nvim_buf_get_lines(0, current_pos[1] - 1, current_pos[1], false)[1]
+  vim.api.nvim_buf_set_lines(0, current_pos[1] - 1, current_pos[1], false, {line_above})
+  vim.api.nvim_buf_set_lines(0, current_pos[1] - 2, current_pos[1] - 1, false, {current_line})
+  
+  vim.api.nvim_win_set_cursor(0, {current_pos[1] - 1, current_pos[2]})
+end
+
+function line_functions.swap_with_line_below()
+  local current_pos = vim.api.nvim_win_get_cursor(0)
+
+  if current_pos[1] == vim.api.nvim_buf_line_count(0) then -- there is nothing below, nothing to swap
+    return 
+  end
+
+  local line_below = vim.api.nvim_buf_get_lines(0, current_pos[1], current_pos[1] + 1, false)[1] -- nvim_win_get_cursor is 1-indexed
+  local current_line = vim.api.nvim_buf_get_lines(0, current_pos[1] - 1, current_pos[1], false)[1]
+  vim.api.nvim_buf_set_lines(0, current_pos[1] - 1, current_pos[1], false, {line_below})
+  vim.api.nvim_buf_set_lines(0, current_pos[1], current_pos[1] + 1, false, {current_line})
+  
+  vim.api.nvim_win_set_cursor(0, {current_pos[1] + 1, current_pos[2]})
+end
+
 return line_functions
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -74,6 +74,8 @@ return require('packer').startup(function()
   keymap('n','<Esc>', '', opts) -- otherwise <Esc> triggers <C-[> for some reason(remaping insert_line_above to another key works as well)
   keymap('','<C-[>', ':lua require "line_functions".insert_line_above()<cr>', opts)
   keymap('','<C-]>', ':lua require "line_functions".insert_line_below()<cr>', opts)
+  keymap('','<C-Up>', ':lua require "line_functions".swap_with_line_above()<cr>', opts)
+  keymap('','<C-Down>', ':lua require "line_functions".swap_with_line_below()<cr>', opts)
 
   use 'sakhnik/nvim-gdb'
 
