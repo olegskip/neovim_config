29ee69aa4798ddaecb37cac2ca47ed14da8788c8
Oleg Skip <skip.cpp.reg@gmail.com>
2022-12-01 00:13:57 +0200

    Small updates to aerial

--- nvim/lua/aerial_config.lua
+++ nvim/lua/aerial_config.lua
@@ -7,7 +7,12 @@ aerial.setup({
 	on_attach = function(bufnr)
 	  vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', {buffer = bufnr})
 	  vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', {buffer = bufnr})
-	end
+	end,
+
+	layout = {
+		min_width = 20,
+		default_direction = "prefer_right",
+	}
 })
 
 vim.keymap.set('n', '<leader>a', '<cmd>AerialToggle!<CR>')
