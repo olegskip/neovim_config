740f8589d820059d7731509a76db371708c846e1
Oleg Skip <skip.cpp.reg@gmail.com>
2022-12-02 00:02:36 +0200

    Now neovim uses global clipboard

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -10,6 +10,7 @@ return require('packer').startup(function()
 	vim.o.tabstop = 4
 	vim.o.shiftwidth = 4
 	vim.g.nocompatible = true
+	vim.o.clipboard = "unnamedplus"
 		
 	use 'tomasiser/vim-code-dark'
 
